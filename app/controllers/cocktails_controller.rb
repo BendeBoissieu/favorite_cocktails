266c03d00799dca8101204a8d65dfdf82dba9c25
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-11 20:41:28 +0200

    with the controllers ok and the index file

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,49 @@
 class CocktailsController < ApplicationController
+
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
-  end
-
-  def new
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def edit
+    @cocktail = Cocktail.find(params[:id])
+
+    @dose=Dose.new
+    @ingredient=Ingredient.all
+
+    @dose.cocktail = @cocktail
+
   end
+
+  def new
+    @cocktail = Cocktail.new
+
+
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to edit_cocktail_path(@cocktail)
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+
+  end
+
+    private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
+
 end
