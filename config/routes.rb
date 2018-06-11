90c35ccd7d2cdb57badd0cf0a24067de01d2eb94
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-11 20:32:39 +0200

    with models rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get 'doses/new'
+
+=begin  get 'doses/new'
   get 'cocktails/index'
   get 'cocktails/show'
   get 'cocktails/new'
   get 'cocktails/edit'
+=end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  resources :cocktails, only: [:index, :show, :edit, :new, :create, :destroy] do
+    resources :doses, only: [:destroy, :create]
+  end
+  resources :ingredients, only: [:create]
+
 end
