2872eb345cb814878ff2344efb7cf798b54f3f14
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-11 20:16:11 +0200

    with the controllers and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/edit'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
