4f1083316a1a94c1aebc9b30ea503ea6158e3e3a
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-11 20:56:23 +0200

    root home to index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'cocktails/edit'
 =end
 
-  root to: 'pages#index'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
